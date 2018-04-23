21a038ec3b7990fe4b7654603b6f74d3dc142955
Viktor <dictor93@gmail.com>
2018-04-23 17:49:22 +0300

    Detect favicon request

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-const http = require('http')
-const port = 3000
+const http = require('http');
+const port = 3000;
 const requestHandler = (request, response) => {
-    console.log(request.url)
-    response.end('Hello Node.js Server!')
+    if(request.url === "/favicon.ico"){
+        console.log("Client need favicon");
+    }else
+        console.log(request.url);
+        response.end('Hello Node.js Server!');
+    }
 }
-const server = http.createServer(requestHandler)
+const server = http.createServer(requestHandler);
 server.listen(port, (err) => {
     if (err) {
-        return console.log('something bad happened', err)
+        return console.log('something bad happened', err);
     }
-    console.log(`server is listening on ${port}`)
+    console.log(`server is listening on ${port}`);
 })
